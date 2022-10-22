6f002ef19512618b5c74dee6b9e13b890c835d10
iton0 <iton442@gmail.com>
2022-10-21 23:23:50 -0400

    Tip calculator

--- tipcalc.py
+++ tipcalc.py
@@ -13,13 +13,11 @@ if percentage <= 20:
     total_bill = float(bill + (bill * percentage / 100))
     print("The total bill including tip - $" + "{:.2f}".format(total_bill))
     print("Thank you for using #insert app name#, have a great day!")
-
 else:
     print("Are you sure this is the right amount? (yes/no): ")
     user_input = input()
     yes_choices = ['yes', 'y']
     no_choices = ['no', 'n']
-
     if user_input.lower() in no_choices:
         print("What percentage tip will you leave? (%) ")
         percentage = int(float(input()))
@@ -29,7 +27,6 @@ else:
         total_bill = float(bill + (bill * percentage / 100))
         print("The total bill including tip - $" + "{:.2f}".format(total_bill))
         print("Thank you for using TipJar!")
-
     elif user_input.lower() in yes_choices:
         print("Total before tip - $" + str(bill))
         tip_amount = "{:.2f}".format((bill * (percentage / 100)))
